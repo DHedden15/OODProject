3bacfc5b416410b38beb8c7d8f602b2c4e1f6f33
Will Hodges <wh03121@georgiasouthern.edu>
2021-04-10 20:43:23 -0400

    added some spell correct

--- test.py
+++ test.py
@@ -2,6 +2,8 @@ import tkinter as tk
 from tkinter import filedialog
 import pickle
 
+from gingerit.gingerit import GingerIt
+
 class DocumentEditor:
 	def __init__(self, master):
 		self.master = master
@@ -15,12 +17,16 @@ class DocumentEditor:
 		self.openbutton = tk.Button(root, text="Open", command=self.open)
 		self.openbutton.pack()
 
+		self.correctbutton = tk.Button(root, text="Correct", command=self.correct)
+		self.correctbutton.pack()
+
 	def save(self):
 		i = self.text.get("1.0","end-1c")
 		filename = filedialog.asksaveasfilename(initialdir = "./",title = "Select file",filetypes = (("PhonetikWrite files","*.pwf"),("all files","*.*")))
 		f = open(filename,'w')
 		f.write(i)
 		f.close()	
+
 	def open(self):
 		filename = filedialog.askopenfilename()
 		if filename == '':
@@ -29,7 +35,14 @@ class DocumentEditor:
 		i = f.read()
 		f.close()
 		self.text.insert("1.0",i)
-		
+
+	def correct(self):
+		i = self.text.get("1.0","end-1c")
+		parser = GingerIt()
+		result = parser.parse(i)['result']
+		self.text.delete('1.0', tk.END)
+		self.text.insert("1.0",result)
+				
 root = tk.Tk()
 root.geometry("500x500")
 root.title("PhonetikWrite")
