a01d1c6018bcc9747cc93e946abef79ccb684a22
Will Hodges <wh03121@georgiasouthern.edu>
2021-04-11 00:16:44 -0400

    threaded spell-check; main menu

--- test.py
+++ test.py
@@ -1,86 +1,126 @@
 import tkinter as tk
 from tkinter import filedialog
-import pickle
 
-from multiprocessing.pool import ThreadPool
+from threading import Thread
 
 from gingerit.gingerit import GingerIt
 
 class DocumentEditor:
-	def __init__(self, root):
-		self.root = root
+	def __init__(self, root, content=''):
+		self.root = root	
+		self.frame = tk.Frame(self.root, height=500,width=500)
+		self.frame.title = "New Document"
+
+		self.top = tk.Frame(self.frame)
+		self.bottom = tk.Frame(self.frame)
+		self.top.pack(side=tk.TOP)
+		self.bottom.pack(side=tk.BOTTOM,fill=tk.BOTH,expand=True)
 		
-		self.checked = ""
-
-		self.scrollbar = tk.Scrollbar(root)
-		self.scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
-
-		self.savebutton = tk.Button(root, text="Save", command=self.save)
-		self.savebutton.pack()
-
-		self.openbutton = tk.Button(root, text="Open", command=self.open)
-		self.openbutton.pack()
-
-		self.correctbutton = tk.Button(root, text="Correct", command=self.correct)
-		self.correctbutton.pack()
+		self.scrollbar = tk.Scrollbar(self.frame)
+		self.scrollbar.pack(in_=self.bottom, side=tk.RIGHT, fill=tk.Y)
 		
-		self.text = tk.Text(root, height=50, width=50)
-		self.text.pack(side=tk.LEFT, fill=tk.BOTH, expand = tk.YES)	
+		self.menubutton = tk.Button(self.frame, text="Main Menu", command=self.menu)
+		self.menubutton.pack(in_=self.top, side=tk.LEFT)
+		
+		self.savebutton = tk.Button(self.frame, text="Save", command=self.save)
+		self.savebutton.pack(in_=self.top, side=tk.LEFT)
+
+		self.correctbutton = tk.Button(self.frame, text="Correct", command=self.correct)
+		self.correctbutton.pack(in_=self.top, side=tk.LEFT)
+		
+		self.text = tk.Text(self.frame, height=50, width=50)
+		self.text.insert("1.0",content)
+		self.text.pack(in_=self.bottom, side=tk.LEFT, fill=tk.BOTH, expand = tk.YES)	
 		self.scrollbar.config(command=self.text.yview)
 		self.text.config(yscrollcommand=self.scrollbar.set)
-	
+		
 		self.text.bind('<period>',self.handle)
+		self.frame.pack()
+		self.frame.pack_propagate(0)
 	
+	def menu(self):
+		self.newWindow = tk.Toplevel(self.root)
+		self.app = MainMenu(self.newWindow)		
+
 	def save(self):
 		i = self.text.get("1.0","end-1c")
 		filename = filedialog.asksaveasfilename(initialdir = "./",title = "Select file",filetypes = (("PhonetikWrite files","*.pwf"),("all files","*.*")))
+		if filename == '':
+			return None
 		f = open(filename,'w')
 		f.write(i)
 		f.close()	
 
-	def open(self):
-		filename = filedialog.askopenfilename()
-		if filename == '':
-			return None
-		f = open(filename,'r')
-		i = f.read()
-		f.close()
-		self.text.insert("1.0",i)
-
-	def ginger_parse(self,parser,inp):
-		return parser.parse(inp)['result']
-
+	def parse(self,inp,returns,thread_n):
+		parser = GingerIt()
+		returns[thread_n] = parser.parse(inp)['result']
 
 	def correct(self):
 		i = self.text.get("1.0","end-1c")
-		i.replace(self.checked,'')
-		i = [i[x:x+500] for x in range(0, len(i), 500)]
-		out = ""
-		parser = GingerIt()
-		n = len(i)
-		pool = ThreadPool(n)
-		results = []
-		for x in i:
-			results.append(pool.apply_async(self.ginger_parse,args=(parser,x)))
-		pool.close()
-		pool.join()
-		results = [r.get() for r in results]
+		print(i)
+		i = i.split('.')
+		i = ['.'.join(i[x:x+10]) for x in range(0, len(i), 10)]		
+		for x in range(0,len(i)):
+			a = i[x]
+			if a.count(' ') > 9:
+				a = a.split(' ')
+				a = [' '.join(a[b:b+5]) for b in range(0, len(a), 5)]
+				i[x] = a[0]
+				i.insert(x+1,a[1])
+		print(i)
+		returns = {}
+
+		threads = []
+		
+		for j in range(0,len(i)):
+			threads.append(Thread(target=self.parse,args=(i[j],returns,j)))
+	
+		for thread in threads:
+			thread.start()
+
+		for thread in threads:
+			thread.join()		
+		
 		out = ''
-		out.join(results)
-		self.checked += out
+		for i in range(0,len(i)):
+			out += returns[i]
+		print(out)
 		self.text.delete('1.0', tk.END)
-		self.text.insert("1.0", self.checked)
+		self.text.insert("1.0", out)
 	
 	def handle(self,event):
 		self.correct()	
 		
-	def run(self):
-		self.root.mainloop()
-	
-root = tk.Tk()
-root.geometry("500x500")
-root.title("PhonetikWrite")
+class MainMenu:
+	def __init__(self,root):
+		self.root = root	
+		self.root.title("PhonetikWrite")
+		
+		new_button = tk.Button(self.root, text="New Document", command=self.new)
+		new_button.pack()
 
-gui = DocumentEditor(root)
+		open_button = tk.Button(self.root, text="Open Document", command=self.open)
+		open_button.pack()
+	
+	def new(self,content=''):
+		self.newWindow = tk.Toplevel(self.root)
+		self.app = DocumentEditor(self.newWindow,content)
+		self.root.withdraw()
+
+	def open(self):
+		filename = filedialog.askopenfilename()
+		if filename == '': 
+			return None
+		f = open(filename,'r')
+		i = f.read()
+		f.close()
+		self.new(i)
+	
+	def run(self):
+		self.root.mainloop()	
+		
+root = tk.Tk()
+
+gui = MainMenu(root)
 
 gui.run()
