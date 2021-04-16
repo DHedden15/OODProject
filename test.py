e7226fc9ff9f81c81509d04ca7b57dd1a93258c8
Will Hodges <wh03121@georgiasouthern.edu>
2021-04-15 20:12:19 -0400

    idk

--- test.py
+++ test.py
@@ -3,17 +3,17 @@ from tkinter import filedialog, font
 from tkinter.font import Font
 from tkinter import ttk
 import time
-from threading import * 
+from threading import *
 import pkg_resources
-from symspellpy import SymSpell, Verbosity
 import pickle
 import multiprocessing
 from jellyfish import *
 from textblob import Word
 import enchant
-from numpy import exp 
+from numpy import exp
 import numpy as np
 from symspellpy import SymSpell, Verbosity
+from spellchecker import SpellChecker
 import pkg_resources
 from copy import deepcopy
 
@@ -27,7 +27,8 @@ class DocumentEditor:
 			dictionary_path = pkg_resources.resource_filename("symspellpy", "frequency_dictionary_en_82_765.txt")
 			self.symspell.load_dictionary(dictionary_path, term_index=0, count_index=1)
 			pickle.dump(self.symspell,open('symspell.pkl','wb'))
-		self.root = root	
+		self.e = enchant.Dict('en')
+		self.root = root
 		self.frame = tk.Frame(self.root, height=500,width=500)
 		self.frame.grid_propagate(False)
 
@@ -40,10 +41,10 @@ class DocumentEditor:
 
 		self.scrollbar = tk.Scrollbar(self.frame)
 		self.scrollbar.grid(row=2,column=1,sticky='NSEW')
-	
+
 		menubar = tk.Menu(self.root)
 		self.root.config(menu=menubar)
-		
+
 		fileMenu = tk.Menu(menubar)
 		menubar.add_cascade(label="File",menu=fileMenu)
 
@@ -62,35 +63,35 @@ class DocumentEditor:
 		self.fontsize.trace('w', self.sizechange)
 		self.sizeMenu = tk.OptionMenu(self.settingsFrame,self.fontsize,*sizes)
 		self.sizeMenu.grid(row=0,column=1)
-		
-		self.corrected = ''	
+
+		self.corrected = ''
 
 		self.line = ttk.Separator(self.frame,orient=tk.HORIZONTAL)
 		self.line.grid(row=1,column=0,sticky='EW')
 
 		fileMenu.add_command(label="Save as...",command=self.save)
 		fileMenu.add_command(label="Correct Spelling...",command=self.correct)
-		fileMenu.add_command(label="Main Menu",command=self.menu)		
+		fileMenu.add_command(label="Main Menu",command=self.menu)
 
 		self.text = tk.Text(self.frame,borderwidth=3)
 		self.text.insert("1.0",content)
 		self.text.grid(row=2,column=0,sticky='NSEW')
 		self.scrollbar.config(command=self.text.yview)
 		self.text.config(font=self.font,undo=True,yscrollcommand=self.scrollbar.set)
-		
+
 		self.text.bind('<period>',self.handle)
 		self.frame.pack(fill='both',expand=True)
 		self.frame.pack_propagate(0)
 
 	def sizechange(self,*args):
 		self.font.config(size=self.fontsize.get())
-	
+
 	def fontchange(self,*args):
 		self.font.config(family=self.fontvariable.get())
 
 	def menu(self):
 		self.newWindow = tk.Toplevel(self.root)
-		self.app = MainMenu(self.newWindow)		
+		self.app = MainMenu(self.newWindow)
 
 	def save(self):
 		i = self.text.get("1.0","end-1c")
@@ -99,31 +100,58 @@ class DocumentEditor:
 			return None
 		f = open(filename,'w')
 		f.write(i)
-		f.close()	
-	
-	def check_word(self,word,mutex):	
-		a = Algo(word,self.symspell,mutex)
+		f.close()
+
+	def check_word(self,word,mutex,e):
+		a = Algo(word,self.symspell,mutex,e)
 		return a.out
-	
+
 	def parse(self,inp,returns,thread_n,mutex):
+		e = enchant.Dict('en')
 		final = inp[-1:]
 		to_check = inp if final != '.' else inp[:-1]
 		if to_check != '':
 			#self.mutex.acquire()
-			inp = self.check_word(to_check,mutex)
+			inp = self.check_word(to_check,mutex,e)
 			#self.mutex.release()
 		returns[thread_n] = inp
- 
+
+	def parse_chunk(self,chunk,returns,j):
+		threads = []
+		ret = {}
+		mutex = Lock()
+		for i in range(0,len(chunk)):
+			t = Thread(target=self.parse,args=(chunk[i],ret,i,mutex))
+			threads.append(t)
+		for thread in threads:
+			thread.start()
+		for thread in threads:
+			thread.join()
+
+		out = list(ret.values())
+		returns[j] = out
+
 	def correct(self):
 		original = self.text.get("1.0","end-1c")
 		orig = deepcopy(original)
+		o = deepcopy(orig)
 		orig = orig.replace(self.corrected,'')
 		i = deepcopy(orig)
 		caps = [x for x in i.split(' ') if x != '']
-		i = [x.lower() for x in caps] 
-		
+		i = [x.lower() for x in caps]
+		#i = [i[j:j+50] for j in range(0,len(i),50)]
+
 		returns = {}
-		
+		'''for j in range(0,len(i)):
+			t = Thread(target=self.parse_chunk,args=(i[j],returns,j))
+			threads.append(t)
+			print(f"Launching thread 1.{j}")
+		for thread in threads:
+			thread.start()
+		for thread in threads:
+			thread.join()'''
+
+
 		threads = []
 		mutex = Lock()
 		for j in range(0,len(i)):
@@ -133,13 +161,14 @@ class DocumentEditor:
 			thread.start()
 
 		for thread in threads:
-			thread.join()		
+			thread.join()
 		out = ''
 		for x in range(0,len(i)):
 			if (caps[x][0].isupper()):
 				out += returns[x].capitalize() + " "
 			else:
-				out += returns[x] + ' '	
+				out += returns[x] + ' '
+
 		out = deepcopy(self.text.get('1.0','end-1c')).replace(orig,out)
 		out = out.replace(' .','.')
 		if out[-1:] == ' ':
@@ -147,21 +176,21 @@ class DocumentEditor:
 		self.text.delete('1.0', tk.END)
 		self.text.insert("1.0", out)
 		self.corrected = out
-	
+
 	def handle(self,event):
-		self.correct()	
-		
+		self.correct()
+
 class MainMenu:
 	def __init__(self,root):
-		self.root = root	
+		self.root = root
 		self.root.title("PhonetikWrite")
-		
+
 		new_button = tk.Button(self.root, text="New Document", command=self.new)
 		new_button.pack()
 
 		open_button = tk.Button(self.root, text="Open Document", command=self.open)
 		open_button.pack()
-	
+
 	def new(self,content='',title='New Document'):
 		self.newWindow = tk.Toplevel(self.root)
 		self.newWindow.title = title
@@ -170,51 +199,57 @@ class MainMenu:
 
 	def open(self):
 		filename = filedialog.askopenfilename()
-		if filename == '': 
+		if filename == '':
 			return None
 		f = open(filename,'r')
 		i = f.read()
 		f.close()
 		self.new(i,filename.split('.')[0])
-	
+
 	def run(self):
-		self.root.mainloop()	
+		self.root.mainloop()
 
 class Algo:
-	def __init__(self,inp,symspell,mutex):
-		self.inp = inp	
-		self.candidates = [x for x,_ in Word(inp).spellcheck()]	
+	def __init__(self,inp,symspell,mutex,e):
+		self.inp = inp
+		self.out = self.inp
+		correct = e.check(self.inp)
+		if not correct:
+			self.correct_word(mutex,symspell,e)
+
+	def correct_word(self,mutex,symspell,e):
+		self.candidates = [x for x,_ in Word(self.inp).spellcheck()]
+		e = e.suggest(self.inp)
 		mutex.acquire()
-		e = enchant.Dict('en').suggest(inp)
-		s = symspell.lookup(inp,Verbosity.ALL,max_edit_distance=2)
+		s = symspell.lookup(self.inp,Verbosity.ALL,max_edit_distance=2)
 		mutex.release()
 		self.candidates += e
 		self.candidates += [x.term for x in s]
 
 		self.options = []
 		for option in self.candidates:
-			comparison = soundex(option) == soundex(inp)
+			comparison = soundex(option) == soundex(self.inp)
 			comparison = comparison and not bool([x for x in [' ','-'] if x in option])
-			if comparison and option[0].islower():			
+			if comparison and option[0].islower():
 				self.options = self.options + [option]
-		
+
 		init = False
 		distances = None
 		for option in self.options:
 			count = self.options.count(option)
 			done = option in distances[:,0] if init == True else False
 			if count >= 2 and not done:
-				dist = -1 * levenshtein_distance(inp,option)
-				dist = dist + jaro_distance(inp,option)
+				dist = -1 * levenshtein_distance(self.inp,option)
+				dist = dist + jaro_distance(self.inp,option)
 				dist = dist * self.options.count(option)
 				pair = np.array([[option,dist]])
 				if init == False:
 					distances = pair
 					init = True
 				else:
-					distances = np.concatenate((distances,pair))	
+					distances = np.concatenate((distances,pair))
 		if init == True:
-			distances[:,1] = self.softmax(distances[:,1])	
+			distances[:,1] = self.softmax(distances[:,1])
 			if len(np.unique(distances[:,1])) < len(distances[:,1]):
 				# If it's a random choice...
 				win = None
@@ -230,13 +265,13 @@ class Algo:
 					else:
 						if freq < c_freq:
 							win = str(word)
-							freq = c_freq	
+							freq = c_freq
 				self.out = win
 			else:
-				self.out = distances[np.argmax(distances,axis=0)[1]][0]	
+				self.out = distances[np.argmax(distances,axis=0)[1]][0]
 		else:
 			self.out = self.inp
-		#mutex.release()
+
 	def softmax(self,arr):
 		arr = exp(arr.astype(float))
 		arr = arr / arr.sum()
